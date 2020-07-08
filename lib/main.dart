1ac16af79df878f79e8df1e617acdfc0f0fdca59
Anurag Raina <rainakalhan90@gmail.com>
2020-07-08 05:50:04 +0530

    Add questionarrie

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,33 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Clicked button");
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("The First App"),
+        body: Column(
+          children: [
+            Text("Answer the questions"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
