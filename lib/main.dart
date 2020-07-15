a966a816b279f0fb7c2ea7d4b3417c321eecf024
Anurag Raina <rainakalhan90@gmail.com>
2020-07-16 05:03:39 +0530

    Format questions by separating in different file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print("Clicked button");
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+
+    print(_questionIndex);
   }
 
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,18 +39,20 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("Answer the questions"),
+            Question(
+              questions[_questionIndex],
+            ),
             RaisedButton(
               child: Text("Answer 1"),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
